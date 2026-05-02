e319a144a9d2c56378486371c3a916201aa129a9
pmlupdates1281-debug <pmlupdates1281@gmail.com>
2026-05-02 23:33:06 +0530

    Index.js

--- Index.js
+++ Index.js
@@ -1,4 +1,37 @@
-headers: {
-  'Authorization': `Bearer ${process.env.API_KEY}`,
-  'Content-Type': 'application/json'
-}
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.get('/go', async (req, res) => {
+    const productUrl = req.query.url;
+
+    if (!productUrl) {
+        return res.send("No URL provided");
+    }
+
+    try {
+        const response = await axios.post(
+            'https://api.earnkaro.com/affiliate/convert',
+            {
+                deal: productUrl,
+                convert_option: "convert_only"
+            },
+            {
+                headers: {
+                    'Authorization': `Bearer ${process.env.API_KEY}`,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+
+        const affiliateLink = response.data.data;
+
+        res.redirect(affiliateLink);
+
+    } catch (error) {
+        console.log(error.response?.data || error.message);
+        res.send("Error generating link");
+    }
+});
+
+app.listen(3000);
